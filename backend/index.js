ecfb0f521ca91c38dfae9c7a8d6c3da982eb273b
smartajay2 <ajayjest2@gmail.com>
2024-05-06 14:25:33 +0400

    login page with react success

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,26 @@
 const express = require("express")
+const cors = require("cors")
 const app= express()
+
+app.use(cors())
+var username="john"
+var password=123
+
+app.use(express.urlencoded({extended:true}))
+
+app.get("/login",function(req,res)
+{  
+  console.log(req.query.username)
+   if(req.query.username===username && req.query.password ==password)
+    {
+      res.send(true)
+    }
+    else{
+      res.send(false)
+    }
+})
+
+
 
 app.listen(4000,function(){
   console.log("Server started....")
